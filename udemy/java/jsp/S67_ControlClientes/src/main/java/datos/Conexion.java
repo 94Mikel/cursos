5ba7ff7d86187b124520378bd73070e7e093e3c8
94Mikel <mikel94.job@gmail.com>
2024-06-07 18:14:30 +0200

    Finalizado seccion 67:  Servlets y JSPs: Proyecto Final Control Cliente Realizado: - Correccion de la conexion en la clase conexion.
    --------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     udemy/java/jsp/S67_ControlClientes/src/main/java/datos/Conexion.java

--- udemy/java/jsp/S67_ControlClientes/src/main/java/datos/Conexion.java
+++ udemy/java/jsp/S67_ControlClientes/src/main/java/datos/Conexion.java
@@ -9,25 +9,30 @@ import org.apache.commons.dbcp2.BasicDataSource;
  * @author mikel
  */
 public class Conexion {
-    
+
     private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    
     private static final String JDBC_USER = "udemy";
     private static final String JDBC_PASSWORD = "chanchito";
-    
+
+    private static BasicDataSource dataSource;
+
     public static BasicDataSource getDataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(JDBC_PASSWORD);
-        ds.setInitialSize(50);
-        return ds;
+
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(JDBC_PASSWORD);
+            dataSource.setInitialSize(50);
+        }
+
+        return dataSource;
     }
-    
+
     public static Connection getConnection() throws SQLException {
         return getDataSource().getConnection();
     }
-    
+
     public static void close(ResultSet rs) {
         try {
             rs.close();
@@ -35,21 +40,21 @@ public class Conexion {
             ex.printStackTrace(System.out);
         }
     }
-    
-    public static void close(PreparedStatement stmt){
+
+    public static void close(PreparedStatement stmt) {
         try {
             stmt.close();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
     }
-    
-    public static void close(Connection conn){
+
+    public static void close(Connection conn) {
         try {
             conn.close();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
     }
-    
+
 }
