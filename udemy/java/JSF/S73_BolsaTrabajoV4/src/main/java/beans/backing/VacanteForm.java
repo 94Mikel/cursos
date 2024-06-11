e4f501360f5283161658221a14d87db7f15df85f
94Mikel <mikel94.job@gmail.com>
2024-06-11 10:12:50 +0200

    Finalizado Sección 74: JavaServerFaces: Mensajes e internacionalización con JSF Realizado: - Internacionalización en JSF
    ---------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     udemy/java/JSF/S73_BolsaTrabajoV3/faces-config.NavData
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/faces-config.NavData
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/nb-configuration.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/pom.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/java/beans/backing/VacanteForm.java
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/java/beans/configuracion/ConfigurationJSF.java
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/java/beans/lifecycle/DebuggerListener.java
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/java/beans/model/Candidato.java
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/resources/META-INF/persistence.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/resources/jsf_en.properties
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/resources/jsf_es.properties
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/resources/log4j2.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/resources/mensajes_en.properties
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/resources/mensajes_es.properties
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/WEB-INF/beans.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/WEB-INF/faces-config.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/WEB-INF/glassfish-web.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/WEB-INF/web.xml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/exito.xhtml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/fallo.xhtml
    	nuevos archivos: udemy/java/JSF/S73_BolsaTrabajoV4/src/main/webapp/index.xhtml

---
+++ udemy/java/JSF/S73_BolsaTrabajoV4/src/main/java/beans/backing/VacanteForm.java
@@ -0,0 +1,51 @@
+package beans.backing;
+
+import beans.model.Candidato;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author mikel
+ */
+@Named
+@RequestScoped
+public class VacanteForm {
+    
+    @Inject
+    private Candidato candidato;
+    
+    Logger log = LogManager.getRootLogger();
+    
+    public VacanteForm(){
+        log.info("Creando el objeto VacanteForm");
+    }
+    
+    public void setCandidato(Candidato candidato){
+        this.candidato = candidato;
+    }
+    
+    public String enviar() {
+        if(this.candidato.getNombre().equals("Juan")){
+            if(this.candidato.getApellido().equals("Perez")){
+                String msg = "Gracias, pero Juan Perez ya trabaja con nosotros.";
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                String componentId = null;//este est un mensaje global
+                facesContext.addMessage(componentId, facesMessage);
+                return "index";
+            }
+            log.info("Entrando al caso de exito");
+            return "exito";
+        }else{
+            log.info("Entrando al caso de fallo");
+            return "fallo";
+        }
+    }
+    
+}
---
+++ udemy/java/JSF/S73_BolsaTrabajoV4/src/main/java/beans/configuracion/ConfigurationJSF.java
@@ -0,0 +1,10 @@
+package beans.configuracion;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+
+@ApplicationScoped
+public class ConfigurationJSF {
+    
+}
