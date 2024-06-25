9ad259c494c9a097921213c660d9f666f4f42c30
94Mikel <mikel94.job@gmail.com>
2024-06-25 11:33:48 +0200

    Seccion 80. Java EE: Java Persistance API(JPA). Realizado: - HolaMundo con JPA en Java EE
    ----------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: S80_HolaMundo-jpa/nb-configuration.xml
    	nuevos archivos: S80_HolaMundo-jpa/pom.xml
    	nuevos archivos: S80_HolaMundo-jpa/src/main/java/S80_HolaMundoJpa.java
    	nuevos archivos: S80_HolaMundo-jpa/src/main/java/mx/com/gm/sga/domain/Persona.java
    	nuevos archivos: S80_HolaMundo-jpa/src/main/java/sql/HolaMundo-jpa.sql
    	nuevos archivos: S80_HolaMundo-jpa/src/main/java/test/ClienteEntidadPersona.java
    	nuevos archivos: S80_HolaMundo-jpa/src/main/resources/META-INF/persistence.xml
    	nuevos archivos: S80_HolaMundo-jpa/src/main/resources/log4j2.xml

---
+++ udemy/java/javaEE/S80_HolaMundo-jpa/src/main/java/S80_HolaMundoJpa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author mikel
+ */
+public class S80_HolaMundoJpa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
