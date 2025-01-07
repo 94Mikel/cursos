dccb89da9e0bb5e75cfd6a817e9853ae6b87596a
94Mikel <mikel94.job@gmail.com>
2025-01-07 12:03:18 +0100

    Hibernate - Asignacion
    - Asignacion con Hibernate - parte 3
    	- Añadir los java beans de dominio.
    		- Alumno
    		- Contacto
    		- Curso
    	- Añadir las asociaciones entre alumno => contacto y curso(many to one)
    
    ------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: leccion04/SgaWebHibernateJPA/src/main/java/mx/com/gm/domain/Alumno.java
    	nuevos archivos: leccion04/SgaWebHibernateJPA/src/main/java/mx/com/gm/domain/Contacto.java
    	nuevos archivos: leccion04/SgaWebHibernateJPA/src/main/java/mx/com/gm/domain/Curso.java

---
+++ udemy/java/hibernate/leccion04/SgaWebHibernateJPA/src/main/java/mx/com/gm/domain/Alumno.java
@@ -0,0 +1,105 @@
+package mx.com.gm.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Alumno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Para que la primaryKey la gestione la base de datos
+    @Column(name = "id_alumno")
+    private Integer idAlumno;
+
+    private String nombre;
+
+    private String apellido;
+
+    //para indicar cual es la columna de union. En este caso id_domicilio
+    //name hacer referencia a la tabla de alumno y referencedColumnName hace referencia a la tabla de domicilio
+    @JoinColumn(name = "id_domicilio", referencedColumnName = "id_domicilio")
+    @ManyToOne//Asociación mucho a uno
+    private Domicilio domicilio;//Obtenemos objetos completos de domicilio
+
+    @JoinColumn(name = "id_contacto", referencedColumnName = "id_contacto")
+    @ManyToOne
+    private Contacto contacto;
+    
+    public Alumno() {
+    }
+
+    public Alumno(Integer idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public Integer getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Integer idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", nombre=" + nombre + ", apellido=" + apellido + ", domicilio=" + domicilio + ", contacto=" + contacto + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.idAlumno);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumno other = (Alumno) obj;
+        return Objects.equals(this.idAlumno, other.idAlumno);
+    }
+
+}
