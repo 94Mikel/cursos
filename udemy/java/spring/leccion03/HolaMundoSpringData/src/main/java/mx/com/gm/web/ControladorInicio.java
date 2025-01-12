a858f639949f03a3967b9023cb6199d2ecad8772
94Mikel <mikel94.job@gmail.com>
2025-01-12 21:29:40 +0100

    Spring data
    Realizado capa DAO para persona conectando a la base de datos postgresql
    Obtener la instacia de personaDao inyectando la dependencia
    Listar las personas obtenidas desde el controlador en index.html
    
    -----------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: HolaMundoSpringData/src/main/java/mx/com/gm/dao/PersonaDao.java
    	modificados:     HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
    	modificados:     HolaMundoSpringData/src/main/resources/templates/index.html

--- udemy/java/spring/leccion03/HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
+++ udemy/java/spring/leccion03/HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -12,7 +12,9 @@ package mx.com.gm.web;
 import java.util.ArrayList;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.dao.PersonaDao;
 import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;//Vamos a agregar la información que queremos compartir con nuestra vista
@@ -22,6 +24,10 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j//Para tener acceso de manera automatica la variable log
 public class ControladorInicio {
     
+    //Para poder inyectar cualquier dependencia objeto que se administrado por el contenedor anotamos la anotación @Autowired
+    //es perecido a Inyect
+    @Autowired
+    private PersonaDao personaDao;
     
     //Por detras se esta ejecutando la tecnologia de los servlets
     //Spring lo esta simplificando todos los procesos
@@ -31,8 +37,11 @@ public class ControladorInicio {
     //La fabrica de spring se encarga de instanciar las clases y nosotro los utilizamos directamente. Ejemplo esta clase Model
     @GetMapping("/")
     public String inicio(Model model){
+        
+        var personas = personaDao.findAll();
+        
         log.info("ejecutando el controlador Spring MVC");
-//        model.addAttribute("personas", personas);
+        model.addAttribute("personas", personas);
         return "index";
     }
     
