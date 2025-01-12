68e0362c93c1111745b082a2fb83aa16962f2ff2
94Mikel <mikel94.job@gmail.com>
2025-01-12 19:33:24 +0100

    Introduccion a Spring MVC y Thymeleaf - Creacion clase de Dominio Persona
    ------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
    	nuevos archivos: HolaSpringThymeleaf/src/main/java/mx/com/gm/domain/Persona.java
    	modificados:     HolaSpringThymeleaf/src/main/resources/templates/index.html

--- udemy/java/spring/leccion02/HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ udemy/java/spring/leccion02/HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -10,6 +10,7 @@ package mx.com.gm;
  * @author mikel
  */
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;//Vamos a agregar la información que queremos compartir con nuestra vista
@@ -34,8 +35,17 @@ public class ControladorInicio {
     public String inicio(Model model){
         var mensaje = "Hola Mundo con Thymeleaf";
         log.info("ejecutando el controlador Spring MVC");
+        
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jperez@mail.com");
+        persona.setTelefono("22554477");
+        
         model.addAttribute("mensaje", mensaje);//Enviar con llave valor
         model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
+        
         return "index";
         //el nombre de la pagina que se va a desplegar en el navegador
         //La tecnologia de thymeleaf por default utilizar archivos html en lugar de archivos como JSP
