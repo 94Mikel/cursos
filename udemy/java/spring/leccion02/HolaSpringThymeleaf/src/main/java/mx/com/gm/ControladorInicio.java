d5a0fe5ced196cd49bd24b7160b7a8bc27ee5360
94Mikel <mikel94.job@gmail.com>
2025-01-12 19:19:27 +0100

    Introduccion a Spring MVC y Thymeleaf - Compartir información con Spring MVC y Thymeleaf
    -------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: HolaSpringThymeleaf/nb-configuration.xml
    	modificados:     HolaSpringThymeleaf/pom.xml
    	modificados:     HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
    	modificados:     HolaSpringThymeleaf/src/main/resources/application.properties
    	modificados:     HolaSpringThymeleaf/src/main/resources/templates/index.html
    	nuevos archivos: readme.md~

--- udemy/java/spring/leccion02/HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ udemy/java/spring/leccion02/HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -10,19 +10,32 @@ package mx.com.gm;
  * @author mikel
  */
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;//Vamos a agregar la información que queremos compartir con nuestra vista
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j//Para tener acceso de manera automatica la variable log
 public class ControladorInicio {
     
+    //Debido a indeccion de dependencias
+    //obtenemos la cadeda desde application.properties
+    @Value("${index.saludo}")
+    private String saludo;
+    
     //Por detras se esta ejecutando la tecnologia de los servlets
     //Spring lo esta simplificando todos los procesos
     
+    
+    //Con injeccion de dependencias obtenemos las clases gracias a las annotaciones
+    //La fabrica de spring se encarga de instanciar las clases y nosotro los utilizamos directamente. Ejemplo esta clase Model
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        var mensaje = "Hola Mundo con Thymeleaf";
         log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje", mensaje);//Enviar con llave valor
+        model.addAttribute("saludo", saludo);
         return "index";
         //el nombre de la pagina que se va a desplegar en el navegador
         //La tecnologia de thymeleaf por default utilizar archivos html en lugar de archivos como JSP
