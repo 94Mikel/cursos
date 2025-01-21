4296d08dd288d32fa32a47e30f24d3fa81bc6ea2
94Mikel <mikel94.job@gmail.com>
2025-01-21 19:19:06 +0100

    Struts2 - Introduccion HolaMundo con Struts 2 - parte 2 	- Creación clase action de struts 	- Crear jsp saludos para saludar desde jsp 	- Modificar index. Cambiar titulo y anadir link
    -------------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: struts2/HolaMundoStruts/src/main/java/beans/HolaMundoAction.java
    	modificados:     struts2/HolaMundoStruts/src/main/webapp/index.html
    	nuevos archivos: struts2/HolaMundoStruts/src/main/webapp/saludos.jsp

---
+++ udemy/java/struts2/HolaMundoStruts/src/main/java/beans/HolaMundoAction.java
@@ -0,0 +1,41 @@
+package beans;
+
+import org.apache.logging.log4j.*;
+
+/*
+Una clase action de struts no necesariamente debe implementar o extender una clase de java.
+Esta es una de las diferencias con la version 1 de struts, así que en este caso únicamente 
+vamos a agregar los siguiente para crear clase de tipo action.
+
+Lo unico que tiene que tener nuestra clase de tipo action es la implementación de un metodo llamado execute.
+
+*/
+
+public class HolaMundoAction {
+    
+    Logger log = LogManager.getLogger(HolaMundoAction.class);
+    
+    /*
+        Definimos un atributo que es el que vamos a utilizar desde el JSP para poder 
+        mandar información hacia el cliente.
+    */
+    
+    private String saludosAtr;
+    
+    public String execute(){
+        log.info("ejecutando motodo execute desde struts2");
+        this.saludosAtr = "Saludos desde Struts2";
+        return "exito";
+    }
+
+    public String getSaludosAtr() {
+        return saludosAtr;
+    }
+
+    public void setSaludosAtr(String saludosAtr) {
+        this.saludosAtr = saludosAtr;
+    }
+    
+    
+    
+}
