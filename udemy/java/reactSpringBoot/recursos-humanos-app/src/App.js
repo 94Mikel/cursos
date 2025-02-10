089c360ca05242c7cac929e37b8fb8143e00b409
94Mikel <mikel94.job@gmail.com>
2025-02-10 17:44:24 +0100

    Sistema de recursos humanos fullStack con ReactJs - Creación App con React de Recursos Humanos - Eliminar ficheros innecesarios - Agregar BootStrap a la App de React
    -------------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/.gitignore
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/README.md
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/package-lock.json
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/package.json
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/public/favicon.ico
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/public/index.html
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/public/manifest.json
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/public/robots.txt
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/src/App.js
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-app/src/index.js

---
+++ udemy/java/reactSpringBoot/recursos-humanos-app/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  //Se utiliza className porque no es html, sino un archivo JavaScript con html embebido
+  return (
+    <div className="container text-center">
+      <h3>Sistema de Recursos Humanos</h3>
+    </div>
+  );
+}
+
+export default App;
