3d35f590066abe1a0afe1c014768cebcd10e4f93
94Mikel <mikel94.job@gmail.com>
2025-02-10 16:58:59 +0100

    Sistema de recursos humanos fullStack con ReactJs y Spring Boot - Listado de Empleados desde el Controlador REST con Spring Boot.
    -------------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: udemy/java/reactSpringBoot/recursos-humanos-spring/nbactions.xml
    	modificados:     udemy/java/reactSpringBoot/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java

--- udemy/java/reactSpringBoot/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ udemy/java/reactSpringBoot/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -1,10 +1,13 @@
 package gm.rh.controlador;
 
+import gm.rh.modelo.Empleado;
 import gm.rh.servicio.EmpleadoServicio;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +40,14 @@ public class EmpleadoControlador {
     //Spring realiza la instacia de EmpleadoServicio por nosotros.
     @Autowired
     private EmpleadoServicio empleadoServicio;
+    
+    //http://localhost:8080/rh-app/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado) -> logger.info(empleado.toString()));
+        return empleados;
+    }
     
     
 }
