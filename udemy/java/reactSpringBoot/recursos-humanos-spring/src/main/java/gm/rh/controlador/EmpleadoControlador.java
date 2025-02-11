1fd6b100e8bf5f2e88c8333299f591ddc195c798
94Mikel <mikel94.job@gmail.com>
2025-02-11 11:14:20 +0100

    Sistema de recursos humanos fullStack con ReactJs y Spring Boot Realizado backEnd: - Actualizar empleado con Spring
    -------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     udemy/java/reactSpringBoot/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java

--- udemy/java/reactSpringBoot/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ udemy/java/reactSpringBoot/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -10,14 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /*
 RestController => para utilizar este controlador dentro de la fabrica de spring. 
@@ -69,6 +62,21 @@ public class EmpleadoControlador {
         if(empleado == null){
             throw new RecursoNoEncontradoExcepcion("No se encontro el id: "+id);
         }
+        return ResponseEntity.ok(empleado);
+    }
+    
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Integer id, @RequestBody Empleado empleadoRecibido){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if(empleado == null){
+            throw new RecursoNoEncontradoExcepcion("El id recibido no existe: "+id);
+        }
+        
+        empleado.setNombre(empleadoRecibido.getNombre());
+        empleado.setDepartamento(empleadoRecibido.getDepartamento());
+        empleado.setSueldo(empleadoRecibido.getSueldo());
+        
+        empleadoServicio.guardarEmpleado(empleado);
         return ResponseEntity.ok(empleado);
     }
     
