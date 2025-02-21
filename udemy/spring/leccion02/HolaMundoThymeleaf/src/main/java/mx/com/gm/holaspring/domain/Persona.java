86dffefb2345f9663a4a8450801cf145f10c9415
94Mikel <mikel94.job@gmail.com>
2025-02-21 17:38:11 +0100

    Introduccion a Spring MVC y Thymeleaf - HolaMundo con Thymeleaf y Spring MVC - Compartir informacion con Spring MVC y Thymeleaf - Creacion clase de dominio Persona - Creacion de una lista de Personas
    -----------------------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: HolaMundoThymeleaf/.gitattributes
    	nuevos archivos: HolaMundoThymeleaf/.gitignore
    	nuevos archivos: HolaMundoThymeleaf/.mvn/wrapper/maven-wrapper.properties
    	nuevos archivos: HolaMundoThymeleaf/mvnw
    	nuevos archivos: HolaMundoThymeleaf/mvnw.cmd
    	nuevos archivos: HolaMundoThymeleaf/pom.xml
    	nuevos archivos: HolaMundoThymeleaf/src/main/java/mx/com/gm/holaspring/ControladorInicio.java
    	nuevos archivos: HolaMundoThymeleaf/src/main/java/mx/com/gm/holaspring/HolaspringApplication.java
    	nuevos archivos: HolaMundoThymeleaf/src/main/java/mx/com/gm/holaspring/domain/Persona.java
    	nuevos archivos: HolaMundoThymeleaf/src/main/resources/application.properties
    	nuevos archivos: HolaMundoThymeleaf/src/main/resources/templates/index.html
    	nuevos archivos: HolaMundoThymeleaf/src/test/java/mx/com/gm/holaspring/HolaspringApplicationTests.java

---
+++ udemy/spring/leccion02/HolaMundoThymeleaf/src/main/java/mx/com/gm/holaspring/domain/Persona.java
@@ -0,0 +1,11 @@
+package mx.com.gm.holaspring.domain;
+
+import lombok.*;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
