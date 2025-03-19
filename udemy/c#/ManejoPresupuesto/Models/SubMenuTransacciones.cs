d1e8eb54628effade2d7e4015b7bea5dc24c73fb
94Mikel <mikel94.job@gmail.com>
2025-03-19 18:29:57 +0100

    Proyecto - Manejo de presupuestos Reportes - Creando un Submenú
    -----------------------------
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     udemy/c#/ManejoPresupuesto/Controllers/TransaccionesController.cs
    	nuevos archivos: udemy/c#/ManejoPresupuesto/Models/SubMenuTransacciones.cs
    	nuevos archivos: udemy/c#/ManejoPresupuesto/Views/Transacciones/Calendario.cshtml
    	nuevos archivos: udemy/c#/ManejoPresupuesto/Views/Transacciones/ExcelReporte.cshtml
    	modificados:     udemy/c#/ManejoPresupuesto/Views/Transacciones/Index.cshtml
    	nuevos archivos: udemy/c#/ManejoPresupuesto/Views/Transacciones/Mensual.cshtml
    	nuevos archivos: udemy/c#/ManejoPresupuesto/Views/Transacciones/Semanal.cshtml
    	nuevos archivos: udemy/c#/ManejoPresupuesto/Views/Transacciones/_Submenu.cshtml

---
+++ udemy/c#/ManejoPresupuesto/Models/SubMenuTransacciones.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Models
+{
+    public enum SubMenuTransacciones
+    {
+        Diario, Semanal, Mensual, Excel, Calendario
+    }
+}
